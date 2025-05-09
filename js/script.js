d52902077e5c0f98936a4cb69c125157cd9e714c
teddYiXiong <teddybabycutie@gmail.com>
2025-05-09 19:52:52 +0800

    Added Debug Features, Test Link Included

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-//Initialize variables
+/*----Initialize variables----*/
+const saveImgButton = document.getElementById('saveImgButton');
 const downloadButton = document.getElementById('downloadButton');
+const testBtn1 = document.getElementById('testBtn1'); //Debug
+const testBtn2 = document.getElementById('testBtn2'); //Debug
+testBtn1.textContent = "Test Link"; //Debug
+testBtn2.textContent = "Test Button 2"; //Debug
+const debugTxtElement = document.getElementById('debugTxt'); //Debug
 
 
 downloadButton.onclick = async function () {
   try {  
-    downloadButton.textContent = 'Click!';
-
     const response = await fetch('images/rxpad/RxPad_2025.pdf');
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -39,4 +43,27 @@ async function downloadFile (inputFile, inputFilename) {
     console.error('Download failed:', error);
     alert('Download failed in downloadFile function');
       }
+}
+
+testBtn1.onclick = async function() {
+  try {  
+    debugTxt.textContent = "TestBtn1";
+
+    const response = await fetch('images/rxpad/RxPad_2025.pdf');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    const blob = await response.blob();
+    
+    const fileUrl = window.URL.createObjectURL(blob);
+    debugTxt.textContent = fileUrl;
+    window.open(fileUrl, '_blank');
+  } catch {
+    console.error('Fetch Request Failed', error);
+    alert('Fetch Failed. Please try again.');
+  }
+}
+
+testBtn2.onclick = function() {
+  debugTxt.textContent = "TestBtn2";
 }
