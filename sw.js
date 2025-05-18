ef7cfe3cad7e7caeef8d035af7a9cf368c0678f3
teddYiXiong <teddybabycutie@gmail.com>
2025-05-18 18:59:31 +0800

    Service Worker Test, Manifest File Updated

--- sw.js
+++ sw.js
@@ -1,23 +1,34 @@
-const CACHE_NAME = 'testcode-v1';
-const ASSETS = [
+const CacheName = 'beta-v0.5';
+const Assets = [
   './',
   'index.html',
-  'RxPad_2025.pdf',
   'src/style.css',
   'src/script.js',
+  'RxPad_2025.pdf',
+  'lib/pdf-lib.min.js',
+  'lib/pdf.mjs',
+  'lib/pdf.worker.mjs',
+  'lib/download.min.js',
   'icons/icon-192.png'
 ];
 
+const addToCache = async (resources) => {
+  const cache = await caches.open(CacheName);
+  await cache.addAll(resources);
+}
+
+const retrieveFromCache = async (request) => {
+  const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+  return fetch(request);
+}
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(addToCache(Assets));
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(retrieveFromCache(event.request));
 });
